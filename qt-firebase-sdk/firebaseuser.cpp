a50bdd8cf7111461143df56f5a8c57ede67c6e47
Patrick Hisni Brataas <patrick@brataas.io>
2020-10-19 00:10:40 +0200

    implemented the user API, except credential calls

--- qt-firebase-sdk/firebaseuser.cpp
+++ qt-firebase-sdk/firebaseuser.cpp
@@ -1,8 +1,9 @@
 #include "firebaseuser.h"
+#include <QUrl>
 
-FirebaseUser::FirebaseUser()
+QString FirebaseUser::name() const
 {
-
+    return QString::fromStdString(m_user->display_name());
 }
 
 FirebaseUser::FirebaseUser(firebase::auth::User* user)
@@ -14,6 +15,149 @@ FirebaseUser::FirebaseUser(firebase::auth::User* user)
 QString FirebaseUser::email() const
 {
     return QString::fromStdString(m_user->email());
+}
+
+QString FirebaseUser::uid() const
+{
+    return QString::fromStdString(m_user->uid());
+}
+
+QUrl FirebaseUser::photo() const
+{
+    return QUrl(QString::fromStdString(m_user->photo_url()));
+}
+
+QString FirebaseUser::providerId() const
+{
+    return QString::fromStdString(m_user->provider_id());
+}
+
+QString FirebaseUser::phoneNumber() const
+{
+    return QString::fromStdString(m_user->phone_number());
+}
+
+bool FirebaseUser::isAnonymous() const
+{
+    return m_user->is_anonymous();
+}
+
+bool FirebaseUser::isEmailVerified() const
+{
+    return m_user->is_email_verified();
+}
+
+QDateTime FirebaseUser::lastSignIn() const
+{
+    return QDateTime::fromMSecsSinceEpoch(static_cast<qint64>(m_user->metadata().last_sign_in_timestamp));
+}
+
+QDateTime FirebaseUser::accountCreated() const
+{
+    return QDateTime::fromMSecsSinceEpoch(static_cast<qint64>(m_user->metadata().creation_timestamp));
+}
+
+void FirebaseUser::updateDisplayName(const QString &displayName)
+{
+    firebase::auth::User::UserProfile profile;
+    profile.display_name = displayName.toStdString().c_str();
+
+    auto result = m_user->UpdateUserProfile(profile);
+    result.OnCompletion([=](const firebase::Future<void>& result){
+        if (result.error() == firebase::auth::kAuthErrorNone) {
+          emit updateDisplayNameSuccess();
+        } else {
+          emit updateDisplayNameError(result.error_message());
+        }
+    });
+}
+
+void FirebaseUser::updatePhoto(const QUrl &url)
+{
+    firebase::auth::User::UserProfile profile;
+    profile.photo_url = url.toString().toStdString().c_str();
+
+    auto result = m_user->UpdateUserProfile(profile);
+    result.OnCompletion([=](const firebase::Future<void>& result){
+        if (result.error() == firebase::auth::kAuthErrorNone) {
+          emit updatePhotoSuccess();
+        } else {
+          emit updatePhotoError(result.error_message());
+        }
+    });
+}
+
+void FirebaseUser::refreshUserData()
+{
+    auto result = m_user->Reload();
+    result.OnCompletion([=](const firebase::Future<void>& result){
+        if (result.error() == firebase::auth::kAuthErrorNone) {
+          emit refreshUserDataSuccess();
+        } else {
+          emit refreshUserDataError(result.error_message());
+        }
+    });
+}
+
+void FirebaseUser::updatePassword(const QString &newPassword)
+{
+    auto result = m_user->UpdatePassword(newPassword.toStdString().c_str());
+    result.OnCompletion([=](const firebase::Future<void>& result){
+        if (result.error() == firebase::auth::kAuthErrorNone) {
+          emit updateEmailSuccess();
+        } else {
+          emit updatePasswordError(result.error_message());
+        }
+    });
+}
+
+void FirebaseUser::sendEmailVerification()
+{
+    auto result = m_user->SendEmailVerification();
+    result.OnCompletion([=](const firebase::Future<void>& result){
+        if (result.error() == firebase::auth::kAuthErrorNone) {
+          emit sendEmailVerificationSuccess();
+        } else {
+          emit sendEmailVerificationError(result.error_message());
+        }
+    });
+}
+
+void FirebaseUser::updateEmail(const QString &email)
+{
+    auto result = m_user->UpdateEmail(email.toStdString().c_str());
+    result.OnCompletion([=](const firebase::Future<void>& result){
+        if (result.error() == firebase::auth::kAuthErrorNone) {
+          emit updateEmailSuccess();
+        } else {
+          emit updateEmailError(result.error_message());
+        }
+    });
+}
+
+void FirebaseUser::token(bool forceRefresh)
+{
+    auto result = m_user->GetToken(forceRefresh);
+    result.OnCompletion([=](const firebase::Future<std::string> result) {
+        emit tokenReturned(QString::fromStdString(*result.result()));
+    });
+}
+
+QVector<FirebaseUser::ProviderData> FirebaseUser::providerData() const
+{
+    QVector<ProviderData> allProviderData;
+    auto data = m_user->provider_data();
+    for (auto entry : data) {
+        ProviderData providerData;
+        providerData.uid = QString::fromStdString(entry->uid());
+        providerData.display_name = QString::fromStdString(entry->display_name());
+        providerData.phone_number = QString::fromStdString(entry->phone_number());
+        providerData.photo_url = QUrl(QString::fromStdString(entry->photo_url()));
+        providerData.provider_id = QString::fromStdString(entry->provider_id());
+        allProviderData.append(providerData);
+    }
+
+    return allProviderData;
 }
 
 void FirebaseUser::remove()
--- qt-firebase-sdk/firebaseuser.h
+++ qt-firebase-sdk/firebaseuser.h
@@ -2,7 +2,8 @@
 #define FIREBASEUSER_H
 
 #include <QObject>
-#include <memory>
+#include <QDateTime>
+#include <QUrl>
 #include "firebase/auth.h"
 #include "firebaseauth.h"
 
@@ -10,13 +11,56 @@ class FirebaseUser : public QObject
 {
     Q_OBJECT
 public:
-    FirebaseUser();
+    struct ProviderData {
+        QString uid;
+        QString email;
+        QString display_name;
+        QUrl photo_url;
+        QString provider_id;
+        QString phone_number;
+    };
+
+    // User info
+    QString name() const;
     QString email() const;
+    QString uid() const;
+    QUrl photo() const;
+    QString providerId() const;
+    QString phoneNumber() const;
+    QVector<FirebaseUser::ProviderData> providerData() const;
+
+    // User status
+    bool isAnonymous() const;
+    bool isEmailVerified() const;
+    QDateTime lastSignIn() const;
+    QDateTime accountCreated() const;
+
+    // User operations
+    void updateDisplayName(const QString& displayName);
+    void updatePhoto(const QUrl& url);
+    void refreshUserData();
+    void updatePassword(const QString& newPassword);
+    void sendEmailVerification();
+    void updateEmail(const QString& email);
+    void token(bool forceRefresh = false);
     void remove();
 
 signals:
+    void updateDisplayNameSuccess();
+    void updateDisplayNameError(const QString& error);
+    void updatePhotoSuccess();
+    void updatePhotoError(const QString& error);
+    void refreshUserDataSuccess();
+    void refreshUserDataError(const QString& error);
+    void updatePasswordSuccess();
+    void updatePasswordError(const QString& error);
+    void sendEmailVerificationSuccess();
+    void sendEmailVerificationError(const QString& error);
+    void updateEmailSuccess();
+    void updateEmailError(const QString& error);
     void removeSuccess();
     void removeError(const QString& error);
+    void tokenReturned(const QString& token);
 
 private:
     friend class FirebaseAuth;
